03a521f0167ebda21c35d83d96d625008218dfec
akeelow <akeelow@gmail.com>
2022-01-11 20:06:26 +0600

    добавил кнопку статистики для админа

--- glassolega.py
+++ glassolega.py
@@ -1,6 +1,12 @@
 from auth import token
 import time
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton
+
+button_stat = KeyboardButton('📊Статистика')
+admin_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(button_stat)
+
 
 
 bot = Bot(token=token)
@@ -8,39 +14,48 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
-    await message.answer("Введите модель телефона.\n" + 
-                         "Пример: Samsung Galaxy A52\n" + 
-                         "Если ввести не полное название модели телефона, тогда бот найдет больше результатов. Каждый результат это совместимые только между собой модели.")
+    if message.from_user.username != "akeelow":
+        await message.answer("Введите модель телефона.\n" + 
+                            "Пример: Samsung Galaxy A52\n" + 
+                            "Если ввести не полное название модели телефона, тогда бот найдет больше результатов. Каждый результат это совместимые только между собой модели.")
+    else:
+        await message.reply("📊Статистика", reply_markup=admin_kb)
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    add_username_to_file(message.from_user.username)
-    full_match = search_for_full_match(message.text)
-    answers = find_element(message.text)
+    if message.text != "📊Статистика":
+        add_username_to_file(message.from_user.username)
+        full_match = search_for_full_match(message.text)
+        answers = find_element(message.text)
 
-    if full_match:
-        await message.answer("📲Совместимые стёкла для «" + message.text + "»:" +
-                             "\n " + "".join(full_match))
-        #await message.answer('📲 ' + '\n📲 '.join(full_match))
+        if full_match:
+            await message.answer("📲Совместимые стёкла для «" + message.text + "»:" +
+                                "\n " + "".join(full_match))
 
-    elif answers:
-        await message.answer('Идет поиск «' + message.text + '»...')
-        for index, answer in enumerate(answers):
-            time.sleep(1)
-            await message.answer('📲' + answer.replace('/', '📲'))
-            if index == 5:
-                break
+        elif answers:
+            await message.answer('Идет поиск «' + message.text + '»...')
+            for index, answer in enumerate(answers):
+                time.sleep(1)
+                await message.answer('📲' + answer.replace('/', '📲'))
+                if index == 5:
+                    break
 
+        else:
+            await message.answer('😔Для *«' + message.text + '»* нет совместимых стёкол.')
     else:
-        await message.answer('😔Для *«' + message.text + '»* нет совместимых стёкол.')
+        await message.answer(get_stat())
+
+def get_stat():
+    users = set(read_file("usernames.txt"))
+    return "Users: {0}".format(len(users))
 
 def add_username_to_file(username):
     with open('usernames.txt','a') as file:
         file.write(username + "\n")
 
 
-def read_file():
-    with open('glassolega.txt', 'r') as fp:
+def read_file(filename):
+    with open(filename, 'r') as fp:
         data = fp.read()
     return data.splitlines()
 
@@ -55,7 +70,7 @@ def search_for_full_match(message_from_user):
 
 def main():
     global list_of_glasses
-    list_of_glasses = read_file()
+    list_of_glasses = read_file("glassolega.txt")
     executor.start_polling(dp, skip_updates=True)
 
 if __name__ == "__main__":
